77244f8cdbc7705e03bb25d87a1388cb21c9276e
Jarbas Pereira de Souza <50222673+jarbassouza@users.noreply.github.com>
2022-01-31 23:34:16 -0300

    Create desconstrucao.js
    31-01-2022

---
+++ scriptJs/desconstrucao.js
@@ -0,0 +1,61 @@
+// Desconstrução (Destruction) em JavaScrip
+
+//Objeto
+let opcoes = {
+	title: "Book",
+	page: 200,
+	species: "scientific",
+}
+// title = propriedade title
+// rest =  objeto com o resto das propriedades
+console.log("--------- Primeiro Exemplo ---------")
+
+let { title, ...rest } = opcoes // agora title e igual a "Book"
+// E rest e igual a {page: 200, species: scientific}
+
+console.log(opcoes) // Retorna o Objeto Literal
+
+console.log(rest.page) // 200
+
+console.log(rest.species) // scientific
+
+const pessoa = {
+	name: "Java",
+	idade: 35,
+}
+
+console.log("--------- Atribuição via Destruction ----------")
+// Atribuir um nome a uma variavel
+const { idade: age } = pessoa
+console.log(age)
+
+console.log("--------- Destruction em Array -----------")
+// Destruction em uma array
+
+const frutas = ["banana", "pera", "maçã"]
+
+const [, , ...resto] = frutas
+
+console.log(resto)
+
+// Usando o operador rest
+let [b, ...asoutrasfrutas] = frutas
+// b retorna o primeiro item e os ... o restante dos itens
+
+console.log(b)
+
+console.log(asoutrasfrutas)
+
+console.log("------- Desestruturação direto na função -----------")
+const fruits = {
+	b: "banana",
+	p: "pera",
+}
+
+function liquidificador({ b, p }) {
+	console.log(b, p)
+}
+
+const result = liquidificador(fruits)
+
+console.log("------- Final ----")
