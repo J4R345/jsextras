168776acef2d56893947f3f7c119c1875894dd1e
Jarbas Pereira de Souza <50222673+jarbassouza@users.noreply.github.com>
2022-02-09 21:37:47 -0300

    Calculos
    09-02-2022

--- calculadora/script.js
+++ calculadora/script.js
@@ -11,12 +11,11 @@ let numeroAnterior
 const operacaoPendente = () => operador !== undefined
 
 const calcular = () => {
-	
-    if (operacaoPendente()) {
+	if (operacaoPendente()) {
 		const numeroAtual = parseFloat(display.textContent)
 		numeroNovo = true
-		const resultado = eval (`${numeroAnterior}${operador}${numeroAtual}`)
-        atualizarDisplay(resultado)
+		const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`)
+		atualizarDisplay(resultado)
 	}
 }
 
@@ -34,14 +33,46 @@ const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent)
 numeros.forEach((numero) => numero.addEventListener("click", inserirNumero))
 
 const selecionarOperador = (evento) => {
+	
 	if (!numeroNovo) {
 		calcular()
 		numeroNovo = true
 		operador = evento.target.textContent
 		numeroAnterior = parseFloat(display.textContent)
-		console.log(operador)
+		
 	}
 }
 operadores.forEach((operador) =>
 	operador.addEventListener("click", selecionarOperador)
 )
+
+/*const ativarIgual = () => {
+	calcular()
+	operador = undefined
+}*/
+
+document.getElementById("igual").addEventListener("click", () => {
+	calcular()
+	operador = undefined
+})
+
+document.getElementById("limparDisplay").addEventListener("click", () => {
+	display.textContent = ''
+})
+
+document.getElementById('limparCalculo').addEventListener('click', () => {
+	
+	display.textContent = ''
+	operador = undefined
+	numeroNovo = true
+	numeroAnterior = undefined
+})
+
+document.getElementById('backspace').addEventListener('click',() => {
+display.textContent = display.textContent.slice(0, -1)	
+})
+
+document.getElementById('inverter').addEventListener('click', () => {
+  numeroNovo = true
+  atualizarDisplay (display.textContent *-1)
+})
