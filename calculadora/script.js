7a97f44e064389dda135a7ef16cff7068e2854ce
Jarbas Pereira de Souza <50222673+jarbassouza@users.noreply.github.com>
2022-02-12 17:15:39 -0300

    Atualizacao
    12-02-2022

--- calculadora/script.js
+++ calculadora/script.js
@@ -12,7 +12,7 @@ const operacaoPendente = () => operador !== undefined
 
 const calcular = () => {
 	if (operacaoPendente()) {
-		const numeroAtual = parseFloat(display.textContent)
+		const numeroAtual = parseFloat(display.textContent.replace(",", "."))
 		numeroNovo = true
 		const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`)
 		atualizarDisplay(resultado)
@@ -21,10 +21,10 @@ const calcular = () => {
 
 const atualizarDisplay = (texto) => {
 	if (numeroNovo) {
-		display.textContent = texto
+		display.textContent = texto.toLocaleString("BR")
 		numeroNovo = false
 	} else {
-		display.textContent += texto
+		display.textContent += texto.toLocaleString("BR")
 	}
 }
 
@@ -33,23 +33,17 @@ const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent)
 numeros.forEach((numero) => numero.addEventListener("click", inserirNumero))
 
 const selecionarOperador = (evento) => {
-	
 	if (!numeroNovo) {
 		calcular()
 		numeroNovo = true
 		operador = evento.target.textContent
-		numeroAnterior = parseFloat(display.textContent)
-		
+		numeroAnterior = parseFloat(display.textContent.replace(",", "."))
 	}
 }
+
 operadores.forEach((operador) =>
 	operador.addEventListener("click", selecionarOperador)
 )
-
-/*const ativarIgual = () => {
-	calcular()
-	operador = undefined
-}*/
 
 document.getElementById("igual").addEventListener("click", () => {
 	calcular()
@@ -57,22 +51,64 @@ document.getElementById("igual").addEventListener("click", () => {
 })
 
 document.getElementById("limparDisplay").addEventListener("click", () => {
-	display.textContent = ''
+	display.textContent = ""
 })
 
-document.getElementById('limparCalculo').addEventListener('click', () => {
-	
-	display.textContent = ''
+document.getElementById("limparCalculo").addEventListener("click", () => {
+	display.textContent = ""
 	operador = undefined
 	numeroNovo = true
 	numeroAnterior = undefined
 })
 
-document.getElementById('backspace').addEventListener('click',() => {
-display.textContent = display.textContent.slice(0, -1)	
+document.getElementById("backspace").addEventListener("click", () => {
+	display.textContent = display.textContent.slice(0, -1)
 })
 
-document.getElementById('inverter').addEventListener('click', () => {
-  numeroNovo = true
-  atualizarDisplay (display.textContent *-1)
+document.getElementById("inverter").addEventListener("click", () => {
+	numeroNovo = true
+	atualizarDisplay(display.textContent * -1)
+})
+
+const existeDecimal = () => display.textContent.indexOf(",") !== -1
+const existeValor = () => display.textContent.length > 0
+
+document.getElementById("decimal").addEventListener("click", () => {
+	if (!existeDecimal()) {
+		if (existeValor()) {
+			atualizarDisplay(",")
+		} else {
+			atualizarDisplay("0,")
+		}
+	}
+})
+
+const mapaTeclado = {
+	0: "tecla0",
+	1: "tecla1",
+	2: "tecla2",
+	3: "tecla3",
+	4: "tecla4",
+	5: "tecla5",
+	6: "tecla6",
+	7: "tecla7",
+	8: "tecla8",
+	9: "tecla9",
+	"/": "operadorDividir",
+	"*": "operadorMultiplicar",
+	"+": "operadorAdicional",
+	"-": "operadorSubtrair",
+	"=": "igual",
+	Enter: "igual",
+	Backspace: "backspace",
+	c: "limparDisplay",
+	Escape: "limparCalculo",
+	",": "decimal",
+}
+
+document.addEventListener("keydown", (evento) => {
+	const tecla = evento.key
+	const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1
+
+	if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click()
 })
