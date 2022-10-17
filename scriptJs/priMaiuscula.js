70da289e5efdf3d3f8355c36164fb74742282c4a
Jarbas Pereira de Souza <jpsdelarue@gmail.com>
2022-10-16 21:25:14 -0300

    Up161020222125

--- scriptJs/priMaiuscula.js
+++ scriptJs/priMaiuscula.js
@@ -1,12 +1,10 @@
-
-
 //Opção 01
 
 const minhaFrase = "O freeCodeCamp é um recurso incrível";
 const palavras = minhaFrase.split(" ");
 
 for (let i = 0; i < palavras.length; i++) {
-    palavras[i] = palavras[i][0].toUpperCase() + palavras[i].substr(1);
+  palavras[i] = palavras[i][0].toUpperCase() + palavras[i].substr(1);
 }
 
 palavras.join(" ");
@@ -15,19 +13,23 @@ console.log(palavras.join(" "));
 
 // Opção 02
 // Com Regex
-/*
-const minhaFrase = "O freeCodeCamp é um recurso incrível";
 
-const fraseFinal = minhaFrase.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase());
+const Frase = "O freeCodeCamp é um recurso incrível";
+
+const fraseFinal = Frase.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase());
 
 console.log(fraseFinal);
 
-*/
+
 
 // Terceira Opção
 // com Regex
-const priMaiuscula = str => {
-    return str.toLowerCase().replace(/(?:^|\s)(?!da|de|do)\S/g, l => l.toUpperCase());
+/*
+const priMaiuscula = (str) => {
+  return str
+    .toLowerCase()
+    .replace(/(?:^|\s)(?!da|de|do)\S/g, (l) => l.toUpperCase());
 };
 
-console.log(priMaiuscula('MARIA Clementina FrancisCA Josefa DA Áustria'))
+console.log(priMaiuscula("MARIA Clementina FrancisCA Josefa DA Áustria"));
+*/
